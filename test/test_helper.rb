dc74eee4784c53285dba011cc8bfd47a2777c431
Maurício Batista <eddloschi@gmail.com>
2014-11-19 19:43:11 -0200

    Compartilfamento de conexão ao bd configurada

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -49,3 +49,13 @@ end
 class ActionController::TestCase
   include Devise::TestHelpers
 end
+
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
